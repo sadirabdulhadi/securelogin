680764d3d74aa6b5d8934ffff3e8a80dd5660c22
Sadir <sadeer11@gmail.com>
2017-01-14 20:15:20 +0000

    okielove

--- securelogin/controller/SignUpController.php
+++ securelogin/controller/SignUpController.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ ini_set('display_errors', 1);
+ ini_set('display_startup_errors', 1);
+ error_reporting(E_ALL);
 	if($_POST){
 		if (hash_equals($_SESSION['token'], $_POST['token'])) {
 			require ('../model/UserModel.php');
